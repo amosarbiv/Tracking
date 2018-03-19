c846a00ccffe9a4b55306c525703464bcb5055db
amos arbiv <amosarbiv@mail.tau.ac.il>
2018-03-19 11:22:37 +0200

    added correlation tracking

---
+++ CrossCorTracker.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2
+class CrossCorTracker():
+    def __init__(self):
+        self.windowPixelJump = 4
+
+    def vcorrcoef(self, target, window):
+            X = np.copy(target)
+            y = np.copy(window)
+            #handling some size issues
+            X_x, X_y = X.shape
+            y_x, y_y = y.shape
+            finalXSize, finalYSize = 0, 0
+            if (X_x > y_x):
+                finalXSize = y_x
+            else:
+                finalXSize = X_x
+            
+            if (X_y > y_y):
+                finalYSize = y_y
+            else:
+                finalYSize = X_y
+
+            X.resize((finalXSize, finalYSize))
+            y.resize((finalXSize, finalYSize))
+            
+            X = X.astype(dtype='float64')
+            y = y.astype(dtype='float64')
+            Xm = X.mean()
+            ym = y.mean()
+            r_num = np.sum((X-Xm)*(y-ym))
+            r_den = np.sqrt(np.sum(np.square(X-Xm))) * \
+                            np.sqrt(np.sum(np.square(y-ym)))
+            r = r_num/r_den
+            return r
+
+    def Track(self, img, target,startXPoint, endXPoint, startYPoint, endYPoint):
+        h = target.shape[0]
+        w = target.shape[1]
+        # the big calculation section calculating the cross corr coefficient
+        #start=time.time()
+        maxX, maxY = 0, 0
+        maxCrossCoeff=0
+        for currentX in range(startXPoint, endXPoint, self.windowPixelJump):
+            for currentY in range(startYPoint, endYPoint, self.windowPixelJump):
+                if ((currentX + w) > endXPoint):
+                    currentX = endXPoint - w
+                if ((currentY + h) > endYPoint):
+                    currentY = endYPoint - h
+                # cv2.rectangle(img, (currentX, currentY), (currentX+w, currentY+h), (255,255,255), 1)
+                # cv2.imshow('in corr', img)
+                # cv2.waitKey(1)
+                crossCoeff=self.vcorrcoef(
+                    target, img[currentY:currentY+h, currentX:currentX+w])
+                if (crossCoeff > maxCrossCoeff):
+                    maxCrossCoeff=crossCoeff
+                    maxX=currentX
+                    maxY=currentY
+        #print(maxCrossCoeff)
+        return (maxCrossCoeff, maxX, maxY)
+        #print(time.time() - start)
--- tracker.py
+++ tracker.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import copy
 
 from utils.backgroungRemove import BackGroundSubtractor, denoise
 from utils.mouse import Selector
@@ -7,35 +8,62 @@ from utils.utils import readImages, save_obj, bb_intersection_over_union
 from utils.trackable import Trackable
 from trackers.canny import bounding_boxes
 from estimators.kalman import KalmanFilter
+import CrossCorTracker
+
 
 def main():
     frames = readImages(args.images_path)
     frame = next(frames)[0]
     back_subtractor = BackGroundSubtractor(frame)
-    # y, x, h, w, window_name = Selector(frame).accuireTarget()
-    y, x, h, w, window_name = 208, 247, 105, 60, 'Image'
+    x,y,w,h, window_name = Selector(frame).accuireTarget()
+    #y, x, h, w, window_name = 208, 247, 105, 60, 'Image'
     corrections = list()
     predictions = list()
     track = list()
     track.append(Trackable(box=(x, y, w, h)))
-    kalman = KalmanFilter(np.zeros((6,1)))
+    kalman = KalmanFilter(np.zeros((6, 1)))
     corrected = track[-1]
     kalman.correct(corrected.center)
     kalman = kalman.predict()
-
+    #TODO Amos's Hacks....need to be removed!!!!
+    new_meas = False
+    target = None
     for frame, *_ in frames:
         last_tracking = track[-1]
         mask = back_subtractor.get_binary(frame)
+        if target is None:
+            target = np.copy(frame[y:y+h, x:x+w])
+            target = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('target', target)
         new_tracking = False
         while not new_tracking:
             if args.method == 'correlation':
-                print('correlation not implemented yet')
-                break
-
-            elif args.method == 'edges':
-                mask = back_subtractor.getMask(frame)
-                trackings = bounding_boxes(denoise(mask))
-                new_tracking = last_tracking.get_closest(trackings)
+                #initialize the needed trackers
+                crossCorTracker = CrossCorTracker.CrossCorTracker()
+                #getting the tracking window from the lest measurment
+                crop, tracking_window = last_tracking.tracking_window(frame)
+                #a print to see the tracking window in the real img
+                cv2.rectangle(frame, tracking_window.top_left(), tracking_window.bottom_right(), (255, 255,255), 1)
+                
+                img_gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#denoise(mask)   
+                #getting the mass of the current img
+                mass = np.sum(img_gray[y:y+h, x:x+w])
+                print("mass:" ,mass)
+                
+                #getting the cross correlation coefficient and top left corner
+                coeff, x, y = crossCorTracker.Track(
+                    img_gray, target, tracking_window.top_left()[0], tracking_window.bottom_right()[0], tracking_window.top_left()[1], tracking_window.bottom_right()[1])
+                print("in tracker coeff is: %f" %coeff)
+                tragetToNewTarget = crossCorTracker.vcorrcoef(target, img_gray[y:y+h, x:x+w])
+                print("tragetToNewTarget: %f" % tragetToNewTarget)
+                # if ( tragetToNewTarget < 0.2):
+                #     target = img_gray[y:y+h, x:x+w]
+                cv2.imshow('target', target)
+                #last tracking and new tracking becomes where we've seen the target
+                last_tracking = tracking_window
+                new_tracking = Trackable(box=(x,y,w,h))
+                crop, tracking_window = corrected.tracking_window(mask)
+                new_meas = tracking_window.center_of_mass(crop)
 
             else:  # center of mass
                 crop, tracking_window = corrected.tracking_window(mask)
@@ -49,21 +77,35 @@ def main():
         predictions.append(prediction)
         if not new_meas:
             new_meas = prediction
-        diagonal = 2 * np.linalg.norm(np.array(track[-1].top_left())-np.array(track[-1].bottom_right()))
+        diagonal = 2 * \
+            np.linalg.norm(
+                np.array(track[-1].top_left())-np.array(track[-1].bottom_right()))
         if new_meas.distance(new_tracking) > diagonal:
             new_meas = new_tracking
         corrected, error = kalman.correct(new_meas.center)
         kalman = kalman.predict()
+
+        corp , tracking_window = corrected.tracking_window(frame)
+        corectedCoeff, x, y = crossCorTracker.Track(
+                    img_gray, target, tracking_window.top_left()[0], tracking_window.bottom_right()[0], tracking_window.top_left()[1], tracking_window.bottom_right()[1])
+        if (corectedCoeff > coeff):
+            print("corrected won!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            last_tracking =  Trackable(box=(x,y,w,h))
+            new_tracking =  Trackable(box=(x,y,w,h))
         track.append(new_tracking)
         corrections.append(corrected)
-        cv2.rectangle(frame, new_tracking.top_left(), new_tracking.bottom_right(), (0,0,255), 1)
-        cv2.rectangle(frame, corrected.top_left(), corrected.bottom_right(), (255,0,255), 1)
-        cv2.rectangle(frame, prediction.top_left(), prediction.bottom_right(), (255,255,255), 1)
+        cv2.rectangle(frame, new_tracking.top_left(),
+                      new_tracking.bottom_right(), (0, 0, 255), 1) # tracker red
+        cv2.rectangle(frame, corrected.top_left(),
+                   corrected.bottom_right(), (255, 0, 255), 1) #corrected prediction purple 
+        cv2.rectangle(frame, prediction.top_left(),
+                      prediction.bottom_right(), (255, 255, 255), 1) #white box kalman prediction
         cv2.imshow(window_name, frame)
-        cv2.waitKey(0)
+        cv2.imshow('imgBlack',mask)
+        cv2.waitKey()
 
-    measurements = [item.as_dict() for item in track]
-    save_obj(measurements, 'measurements')
+    #measurements = [item.as_dict() for item in track]
+    #save_obj(measurements, 'measurements')
 
 
 if __name__ == '__main__':
@@ -71,6 +113,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--images-path')
     parser.add_argument('-s', '--frame-size', type=int, default=200)
-    parser.add_argument('-m', '--method', type=str, choices=['mass', 'correlation', 'edges'], default='mass')
+    parser.add_argument('-m', '--method', type=str,
+                        choices=['mass', 'correlation', 'edges'], default='mass')
     args = parser.parse_args()
     main()
